6ca16bd414664387436b70180a9e47fcddddcae2
Cure232 <timtim2004232@gmail.com>
2023-12-02 23:09:09 +0400

    csv creation script

--- main.py
+++ main.py
@@ -5,11 +5,18 @@ import numpy as np
 import pandas as pd
 
 columns = ["Absolute path", "Relative path", "Class"]
-data = [
-["askdsalda", "fdgdg", "Item1"],
-["askdsalda123123", "fdgdg21312", "Item2"],
-["3", "sae3123", "Item3"]
-]
+data = []
+rel_path1 = r'dataset\bay horse'
+rel_path2 = r'dataset\zebra'
+
+name_list1 = os.listdir(rel_path1)
+name_list2 = os.listdir(rel_path2)
+
+for name in name_list1:
+    data.append([os.path.abspath(rel_path1 + name), rel_path1 + name, "Bay horse"])
+for name in name_list2:
+    data.append([os.path.abspath(rel_path2 + name), rel_path2 + name, "Zebra"])
+
 
 df = pd.DataFrame(data, columns=columns)
-df.to_csv('annotation.csv')
+df.to_csv('annotation.csv', sep=";")
