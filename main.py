519f5052f67019bd813971f4abbbdf912594fd27
Cure232 <timtim2004232@gmail.com>
2023-12-18 18:17:20 +0400

    File rename, preparing ground for lab3

--- main.py
+++ main.py
@@ -1,15 +1,31 @@
 import os
-
 import pandas as pd
 
 
 def save_as_csv(to_save: list[list[str]], columns: list[str], relpath: str) -> None:
+    """Saves given data table as csv
+
+    Args:
+        to_save (list[list[str]]): Table (matrix) of strings to be saved, formatted as in scan_dataset.
+
+        columns (list[str]): Name of data's columns.
+
+        relpath (str): Relative path where data is to be saved.
+    """
     df = pd.DataFrame(to_save, columns=columns)
     df.to_csv(relpath, sep=";")
     print(f'Successfully saved in {relpath}')
 
 
 def scan_dataset(folder_paths: list[str]) -> list[list[str]]:
+    """This function scans given datasets and returns them in a form that can be saved as csv.
+
+    Args:
+        folder_paths (list[str]): Paths to datasets to be scanned.
+
+    Returns:
+        list[list[str]]: Data in a form of table of strings.
+    """
     result = []
     for folder in folder_paths:
         item_class = folder.split('\\')[-1]
--- task5.py
+++ task5.py
@@ -1,10 +1,16 @@
-import csv
+from task2 import scan_annotation
 
+data = []
 
 class HorseIterator:
     data_position = 0
 
     def __init__(self, limit):
+        """Initializes HorseIterator that can iterate through data variable from this script.
+
+        Args:
+            limit (int): Max number of iterations.
+        """
         self.limit = limit
         self.counter = 0
 
@@ -12,6 +18,14 @@ class HorseIterator:
         return self
 
     def __next__(self):
+        """Returns path to next item, increments the counter.
+
+        Raises:
+            StopIteration: Standard iterator signal.
+
+        Returns:
+            str: Absolute path to next Horse image.
+        """
         if self.counter < self.limit:
             for i in range(self.data_position, len(data)):
                 if data[i][2] == "bay horse":
@@ -26,6 +40,11 @@ class ZebraIterator:
     data_position = 0
 
     def __init__(self, limit):
+        """Initializes HorseIterator that can iterate through data variable from this script.
+
+        Args:
+            limit (int): Max number of iterations.
+        """
         self.limit = limit
         self.counter = 0
 
@@ -33,6 +52,14 @@ class ZebraIterator:
         return self
 
     def __next__(self):
+        """Returns path to next item, increments the counter.
+
+        Raises:
+            StopIteration: Standard iterator signal.
+
+        Returns:
+            str: Absolute path to next Zebra image.
+        """
         if self.counter < self.limit:
             for i in range(self.data_position, len(data)):
                 if data[i][2] == "zebra":
@@ -43,21 +70,11 @@ class ZebraIterator:
             raise StopIteration
 
 
-def scan_annotation(annotation_path: str) -> list[list[str]]:
-    with open(annotation_path, 'r', newline='') as csvfile:
-        filereader = csv.reader(csvfile, delimiter=';', quotechar='|')
-        result = []
-        for row in filereader:
-            row.pop(0)
-            result.append(row)
-        return result 
-
-
 if __name__ == '__main__':
     data = scan_annotation('annotation.csv')
     s_iter1 = HorseIterator(3)
     s_iter2 = ZebraIterator(5)
-    s_iter2.__next__
+    print(s_iter2.__next__())
     for val in s_iter1:
         print(val)
 
